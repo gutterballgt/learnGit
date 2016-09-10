84eeb81207a8a7da2da76ca531f4c95e618f62f5
Gary Tam <garytam@Garys-Air.hitronhub.home>
2016-09-10 17:39:59 -0400

    initial commit

---
+++ StringPOC.java
@@ -0,0 +1,14 @@
+package ca.gtConsulting.poc;
+
+public class StringPOC {
+
+	public static void main(String[] args){
+		String str = "This is %s a test";
+		String formattedStr = String.format(str, "Very Big");
+		
+		System.out.println(formattedStr);
+		
+		
+		str = "This is %s more tests %s";
+	}
+}
